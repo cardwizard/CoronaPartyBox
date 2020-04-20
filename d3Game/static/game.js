aa9a138838eff09589bad545fd969748e1cd9f81
ameyabp <ameyap@umd.edu>
2020-04-20 16:48:17 -0400

    Working game with user input

--- d3Game/static/game.js
+++ d3Game/static/game.js
@@ -11,6 +11,9 @@ var barThickness = 10;
 
 var ballRadius = 7;
 
+// last user input timestamp
+var time = Date.now();
+
 // variables for svg objects
 var svg;
 var barGroup, renderedBars, ballGroup, renderedBall;
@@ -23,7 +26,7 @@ svg = d3.select("#gameDiv").append("svg").attr("id", "gameSvg").attr("width", sv
 // initialize the game
 setup();
 
-// set the render chain for the svg elements and render them once
+// render the svg elements once
 render();
 
 function setup() {
@@ -48,26 +51,31 @@ function setup() {
                     .alphaDecay(0)   // no decay factor, let the simulation run till it is stopped explicitly
                     .velocityDecay(0)
                     .stop();
+
+    // register key press listener
+    d3.select("body").on("keydown", moveBar);
 }
 
 function reset() {
-    var bar;
-    
-    bar = barElements[0];
-    bar.cx = svgWidth/2;
-    bar.cy = svgHeight-bar.thickness/2;
+    // barB
+    barB = barElements[0];
+    barB.cx = svgWidth/2;
+    barB.cy = svgHeight-barB.thickness/2;
 
-    bar = barElements[1];
-    bar.cx = svgWidth - bar.thickness/2;
-    bar.cy = svgHeight/2;
+    // barR
+    barR = barElements[1];
+    barR.cx = svgWidth - barR.thickness/2;
+    barR.cy = svgHeight/2;
 
-    bar = barElements[2];
-    bar.cx = svgWidth/2;
-    bar.cy = bar.thickness/2;
+    // barT
+    barT = barElements[2];
+    barT.cx = svgWidth/2;
+    barT.cy = barT.thickness/2;
 
-    bar = barElements[3];
-    bar.cx = bar.hickness/2;
-    bar.cy = svgHeight/2;
+    // barL
+    barL = barElements[3];
+    barL.cx = barL.thickness/2;
+    barL.cy = svgHeight/2;
 
     ball.x = svgWidth/2;
     ball.y = svgHeight/2;
@@ -85,46 +93,46 @@ function render() {
     // clear the old svg groups
     d3.select("#gameSvg").selectAll("g").remove();
 
-    // set the render function chain for bars
+    // render teh bars
     barGroup = d3.select("#gameSvg").append("g").attr("class", "bars").selectAll("rect").data(barElements);
     renderedBars = barGroup.enter()
-                                .append("rect")
-                                .attr("class", function (d) {return d.class})
-                                .attr("id", function (d) {return d.id})
-                                .attr("x", function (d) {
-                                    if (d.id == "barB" || d.id == "barT") {
-                                        return d.cx - d.length/2;
-                                    }
-                                    else {
-                                        return d.cx - d.thickness/2;
-                                    }
-                                })
-                                .attr("y", function(d) {
-                                    if (d.id == "barB" || d.id == "barT") {
-                                        return d.cy - d.thickness/2;
-                                    }
-                                    else {
-                                        return d.cy - d.length/2;
-                                    }
-                                })
-                                .attr("width", function (d) {
-                                    if (d.id == "barB" || d.id == "barT") {
-                                        return d.length;
-                                    }
-                                    else {
-                                        return d.thickness;
-                                    }
-                                })
-                                .attr("height", function (d) {
-                                    if (d.id == "barB" || d.id == "barT") {
-                                        return d.thickness;
-                                    }
-                                    else {
-                                        return d.length;
-                                    }
-                                });
+                            .append("rect")
+                            .attr("class", function (d) {return d.class})
+                            .attr("id", function (d) {return d.id})
+                            .attr("x", function (d) {
+                                if (d.id == "barB" || d.id == "barT") {
+                                    return d.cx - d.length/2;
+                                }
+                                else {
+                                    return d.cx - d.thickness/2;
+                                }
+                            })
+                            .attr("y", function(d) {
+                                if (d.id == "barB" || d.id == "barT") {
+                                    return d.cy - d.thickness/2;
+                                }
+                                else {
+                                    return d.cy - d.length/2;
+                                }
+                            })
+                            .attr("width", function (d) {
+                                if (d.id == "barB" || d.id == "barT") {
+                                    return d.length;
+                                }
+                                else {
+                                    return d.thickness;
+                                }
+                            })
+                            .attr("height", function (d) {
+                                if (d.id == "barB" || d.id == "barT") {
+                                    return d.thickness;
+                                }
+                                else {
+                                    return d.length;
+                                }
+                            });
 
-    // set the render function chain for ball
+    // render the ball
     ballGroup = d3.select("#gameSvg").append("g").attr("class", "balls").selectAll("circle").data([ball]);
     renderedBall = ballGroup.enter()
                                 .append("circle")
@@ -136,12 +144,12 @@ function render() {
 }
 
 function startGame() {
-    reset();
     simulation.alpha(1).restart();
 }
 
 function stopGame() {
     simulation.stop();
+    reset();
 }
 
 function ticked() {
@@ -149,50 +157,107 @@ function ticked() {
     renderedBall.attr("cx", function(d) {return d.x;})
                 .attr("cy", function(d) {return d.y;});
 
-    // do not update the bar position here, it will be updated based on the user input
+    // update the bar position here
+    renderedBars.attr("x", function (d) {
+                    if (d.id == "barB" || d.id == "barT") {
+                        return d.cx - d.length/2;
+                    }
+                    else {
+                        return d.cx - d.thickness/2;
+                    }
+                })
+                .attr("y", function(d) {
+                    if (d.id == "barB" || d.id == "barT") {
+                        return d.cy - d.thickness/2;
+                    }
+                    else {
+                        return d.cy - d.length/2;
+                    }
+                });
+}
+
+function moveBar() {
+    var userInput = d3.event.keyCode;
+    var moveStep = 5;
+
+    if (Date.now() - time < 25 && moveStep < 50) {
+        moveStep += 5;
+        time = Date.now();
+    }
+    else {
+        moveStep = 5;
+    }
+
+    // use the user input to set the bar position
+    if ((userInput == 37) && (barElements[0].cx - 2 > barElements[0].length/2)) {
+        // left arrow
+        barElements[0].cx -= moveStep;
+    }
+    if ((userInput == 39) && (barElements[0].cx + 2 < svgWidth - barElements[0].length/2)) {
+        // right arrow
+        barElements[0].cx += moveStep;
+    }
 }
 
 function reboundForce(alpha) {
     var bar;
-    // check if the ball collides with any bar and rebound the ball if it does
+    // check if the ball collides with any bar and 
+    // rebound the ball if it does
+    // or end the game if it is dropped
 
     // bottom bar
     bar = barElements[0];
-    if (ball.x >= (bar.cx - bar.length/2 - ballRadius) &&
-        ball.x <= (bar.cx + bar.length/2 + ballRadius) &&
-        ball.y >= (bar.cy - bar.thickness/2 - ballRadius)) 
-    {
-        // ball is colliding with bottom bar, flip the y component of its velocity
-        ball.vy *= -1;
+    if (ball.y >= (bar.cy - bar.thickness/2 - ballRadius)) {
+        // ball is below the ground
+        if (ball.x >= (bar.cx - bar.length/2 - ballRadius) && ball.x <= (bar.cx + bar.length/2 + ballRadius)) {
+            // ball is colliding with bottom bar, flip the y component of its velocity
+            ball.vy *= -1;
+        }
+        else {
+            // ball is not colliding with the bottom bar, ball is dropped, reset playground
+            stopGame();
+        }
     }
 
     // right bar
     bar = barElements[1];
-    if (ball.y >= (bar.cy - bar.length/2 - ballRadius) &&
-        ball.y <= (bar.cy + bar.length/2 + ballRadius) &&
-        ball.x >= (bar.cx - bar.thickness/2 - ballRadius)) 
-    {
-        // ball is colliding with bottom bar, flip the y component of its velocity
-        ball.vx *= -1;
+    if (ball.x >= (bar.cx - bar.thickness/2 - ballRadius)) {
+        // ball is beyond the right wall
+        if (ball.y >= (bar.cy - bar.length/2 - ballRadius) && ball.y <= (bar.cy + bar.length/2 + ballRadius)) {
+            // ball is colliding with right bar, flip the x component of its velocity
+            ball.vx *= -1;
+        }
+        else {
+            // ball is not colliding with the right bar, ball is dropped, reset playground
+            stopGame();
+        }
     }
 
     // top bar
     bar = barElements[2];
-    if (ball.x >= (bar.cx - bar.length/2 - ballRadius) &&
-        ball.x <= (bar.cx + bar.length/2 + ballRadius) &&
-        ball.y <= (bar.cy + bar.thickness/2 + ballRadius)) 
-    {
-        // ball is colliding with bottom bar, flip the y component of its velocity
-        ball.vy *= -1;
+    if (ball.y <= (bar.cy + bar.thickness/2 + ballRadius)) {
+        // ball is above the ceiling
+        if (ball.x >= (bar.cx - bar.length/2 - ballRadius) && ball.x <= (bar.cx + bar.length/2 + ballRadius)) {
+            // ball is colliding with top bar, flip the y component of its velocity
+            ball.vy *= -1;
+        }
+        else {
+            // ball is not colliding with the top bar, ball is dropped, reset playground
+            stopGame();
+        }
     }
 
     // left bar
     bar = barElements[3];
-    if (ball.y >= (bar.cy - bar.length/2 - ballRadius) &&
-        ball.y <= (bar.cy + bar.length/2 + ballRadius) &&
-        ball.x <= (bar.cx + bar.thickness/2 + ballRadius)) 
-    {
-        // ball is colliding with bottom bar, flip the y component of its velocity
-        ball.vx *= -1;
+    if (ball.x <= (bar.cx + bar.thickness/2 + ballRadius)) {
+        // ball is beyond the left wall
+        if (ball.y >= (bar.cy - bar.length/2 - ballRadius) && ball.y <= (bar.cy + bar.length/2 + ballRadius)) {
+            // ball is colliding with bottom bar, flip the y component of its velocity
+            ball.vx *= -1;
+        }
+        else {
+            // ball is not colliding with the left bar, ball is dropped, reset playground
+            stopGame();
+        }
     }
 }
